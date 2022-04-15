c92fbff263aead61694d07dba5f2e5aa55cf8e50
Rob DiMarzo <robdimarzo@gmail.com>
2022-04-15 08:38:47 -0400

    Initial commit
    project setup, svgs, routing

---
+++ src/components/NavButton/NavButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { A } from 'hookrouter';
+
+import Rune from '../Rune/Rune';
+import './NavButton.css';
+
+function NavButton(props) {
+    const href= '/' + props.name.toLowerCase();
+    return (
+        <A href={href} className={href === window.location.pathname ? 'active' : null}>
+            <Rune name={props.name} size="30"/>
+            <span>{props.name}</span>
+        </A>
+    )
+  }
+  
+  export default NavButton;
+  
